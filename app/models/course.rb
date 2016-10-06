b0533a879ff782fdef81ab5ef49a4ad1571b665f
ZhouWei <chunwei123hu@163.com>
2016-10-06 03:15:48 +0000

    update model

--- app/models/course.rb
+++ app/models/course.rb
@@ -5,8 +5,12 @@ class Course < ActiveRecord::Base
 
   belongs_to :teacher, class_name: "User"
 
-  validates :name, :course_type, :course_time, :course_week,
-            :class_room, :credit, :teaching_type, :exam_type, presence: true, length: {maximum: 50}
-
-
+  validates :name, :course_time, :course_week,
+            :class_room, :credit, :teaching_type,  
+            presence: true, length: {maximum: 50}
+  validates :course_type,inclusion: { in: %w(专业普及课 专业讨论课 专业核心课 公共必修课，公共选修课),
+            message: "%{value} is not a valid course type" },presence: true
+  validates :exam_type,inclusion: { in: %w( 课堂开卷 闭卷考试 课上讨论，大作业，其他),
+            message: "%{value} is not a valid exam type" },presence: true
 end
+          
